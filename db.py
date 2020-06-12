d33fcfc16129c051a9defcef51c9f2e970808de9
vishnupriyagoud <vishnupriyagangarapu@gmail.com>
2020-06-12 11:23:28 +0530

    changes

--- db.py
+++ db.py
@@ -1,12 +1,12 @@
 from flask import jsonify
-import pymysql
 from decimal import Decimal
+import pymysql
 
-def query(querystr , return_json=True):
-    connection=pymysql.connect(host='cosc-skillup.cxgok3weok8n.ap-south-1.rds.amazonaws.com',
-                                user="admin",
-                                password="coscskillup",
-                                db="testapi",
+def query(querystr,return_json=True):
+    connection=pymysql.connect( host='cosc-skillup.cxgok3weok8n.ap-south-1.rds.amazonaws.com',
+                                user='admin',
+                                password='coscskillup',
+                                db='testapi',
                                 cursorclass=pymysql.cursors.DictCursor )
     connection.begin()
     cursor=connection.cursor()
@@ -16,15 +16,13 @@ def query(querystr , return_json=True):
     cursor.close()
     connection.close()
     if return_json:
-        return jsonify(result)
+         return jsonify(result)
     else:
         return result
-
 
 def encode(data):
     for row in data:
         for key,value in row.items():
-            if isinstance(value, Decimal):
+            if isinstance(value,Decimal):
                 row[key]=str(value)
-    #print(query("""SELECT * FROM EMP;"""))
     return data
