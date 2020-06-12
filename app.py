27f2b151a2183686063e8544975bc4e81ecc587c
vishnupriyagoud <vishnupriyagangarapu@gmail.com>
2020-06-12 12:57:39 +0530

    final

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-from flask import Flask
+from flask import Flask,jsonify
 from flask_restful import Api
-from resources.emp import Emp
+from flask_jwt_extended import JWTManager
+from resources.emp import Emp,EmpLogin
 
 app=Flask(__name__)
+app.config['PROPAGATE_EXCEPTIONS']=True
+app.config['JWT_SECRET_KEY']='coscskillup'
+api=Api(app)
+jwt=JWTManager(app)
 
-api= Api(app)
+@jwt.unauthorized_loader
+def missing_token_callback(error):
+    return jsonify({
+        'error': 'authorization_required',
+        "description": "Request does not contain an access token."
+    }), 401
+
+@jwt.invalid_token_loader
+def invalid_token_callback(error):
+    return jsonify({
+        'error': 'invalid_token',
+        'message': 'Signature verification failed.'
+    }), 401
+
 api.add_resource(Emp,'/emp')
+api.add_resource(EmpLogin,'/login')
 
-
-app.run()
+if __name__=='__main__':
+    app.run()
--- resources/emp.py
+++ resources/emp.py
@@ -1,40 +1,40 @@
-from flask_restful import Resource, reqparse
-from db import query 
+from flask_restful import Resource,reqparse
+from werkzeug.security import safe_str_cmp
+from flask_jwt_extended import create_access_token,jwt_required
+from db import query
 
 class Emp(Resource):
+    @jwt_required
     def get(self):
         parser=reqparse.RequestParser()
-        parser.add_argument('empno',type=int , required=True, help= "cant leave blank")
+        parser.add_argument('empno',type=int,required=True,help="empno cannot be left blank!")
         data=parser.parse_args()
-        print(data)
         try:
-            return query(f"""SELECT * FROM testapi.emp WHERE empno={data['empno']};""")
+            return query(f"""SELECT * FROM testapi.emp WHERE empno={data['empno']}""")
         except:
-            return {"message":"error connecting table"},500
+            return {"message":"There was an error connecting to emp table."},500
 
+    @jwt_required
     def post(self):
         parser=reqparse.RequestParser()
-        parser.add_argument('empno',type=int , required=True, help= "cant leave blank")
-        parser.add_argument('ename',type=str , required=True, help= "cant leave blank")
-        parser.add_argument('job',type=str, required=True, help= "cant leave blank")
-        parser.add_argument('mgr',type=int , required=True, help= "cant leave blank")
-        parser.add_argument('hiredate',type=str , required=True, help= "cant leave blank")
-        parser.add_argument('sal',type=str , required=True, help= "cant leave blank")
-        parser.add_argument('comm',type=str )
-        parser.add_argument('deptno',type=int , required=True, help= "cant leave blank")
-        parser.add_argument('pass',type=str , required=True, help= "cant leave blank")
-
+        parser.add_argument('empno',type=int,required=True,help="empno cannot be left blank!")
+        parser.add_argument('ename',type=str,required=True,help="empname cannot be left blank!")
+        parser.add_argument('job',type=str,required=True,help="job cannot be left blank!")
+        parser.add_argument('mgr',type=int,required=True,help="mgr cannot be left blank!")
+        parser.add_argument('hiredate',type=str,required=True,help="hiredate cannot be left blank!")
+        parser.add_argument('sal',type=str,required=True,help="sal cannot be left blank!")
+        parser.add_argument('comm',type=str)
+        parser.add_argument('deptno',type=int,required=True,help="deptno cannot be left blank!")
+        parser.add_argument('pass',type=str,required=True,help="password cannot be left blank!")
         data=parser.parse_args()
         try:
-            x = query(f"""SELECT * FROM testapi.emp WHERE empno={data['empno']};""", return_json=False)
-            if len(x)>0:
-                return { "message":"empno already exists"}
+            x=query(f"""SELECT * FROM testapi.emp WHERE empno={data['empno']}""",return_json=False)
+            if len(x)>0: return {"message":"An emp with that empno already exists."},400
         except:
-            return {"message":"error inserting into table"},500
-
+            return {"message":"There was an error inserting into emp table."},500
         if data['comm']!=None:
             try:
-                query(f"""INSERT INTO testapi.emp values({data['empno']},
+                query(f"""INSERT INTO testapi.emp VALUES({data['empno']},
                                                         '{data['ename']}',
                                                         '{data['job']}',
                                                         {data['mgr']},
@@ -42,21 +42,51 @@ class Emp(Resource):
                                                         '{data['sal']}',
                                                         '{data['comm']}',
                                                         {data['deptno']},
-                                                        '{data['pass']}');""")
+                                                        '{data['pass']}')""")
             except:
-                return {"message":"error inserting into table"},500
-            return {"message":"Success"},201      
+                return {"message":"There was an error inserting into emp table."},500
+            return {"message":"Successfully Inserted."},201
         else:
             try:
-                query(f"""INSERT INTO testapi.emp (empno, ename, job, mgr, hiredate, sal, deptno, pass)
-                                                    values({data['empno']},
+                query(f"""INSERT INTO testapi.emp (empno,ename,job,mgr,hiredate,sal,deptno,pass)
+                                                    VALUES({data['empno']},
                                                         '{data['ename']}',
                                                         '{data['job']}',
                                                         {data['mgr']},
                                                         '{data['hiredate']}',
                                                         '{data['sal']}',
                                                         {data['deptno']},
-                                                        '{data['pass']}');""")
+                                                        '{data['pass']}')""")
             except:
-                return {"message":"error inserting into table"},500
-            return {"message":"Success"},201 
+                return {"message":"There was an error inserting into emp table."},500
+            return {"message":"Successfully Inserted."},201
+
+class User():
+    def __init__(self,empno,ename,password):
+        self.empno=empno
+        self.ename=ename
+        self.password=password
+
+    @classmethod
+    def getUserByEname(cls,ename):
+        result=query(f"""SELECT empno,ename,pass FROM emp WHERE ename='{ename}'""",return_json=False)
+        if len(result)>0: return User(result[0]['empno'],result[0]['ename'],result[0]['pass'])
+        return None
+
+    @classmethod
+    def getUserByempno(cls,empno):
+        result=query(f"""SELECT empno,ename,pass FROM emp WHERE empno='{empno}'""",return_json=False)
+        if len(result)>0: return User(result[0]['empno'],result[0]['ename'],result[0]['pass'])
+        return None
+
+class EmpLogin(Resource):
+    def post(self):
+        parser=reqparse.RequestParser()
+        parser.add_argument('ename',type=str,required=True,help="empname cannot be left blank!")
+        parser.add_argument('pass',type=str,required=True,help="password cannot be left blank!")
+        data=parser.parse_args()
+        user=User.getUserByEname(data['ename'])
+        if user and safe_str_cmp(user.password,data['pass']):
+            access_token=create_access_token(identity=user.empno,expires_delta=False)
+            return {'access_token':access_token},200
+        return {"message":"Invalid Credentials!"}, 401
